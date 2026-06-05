153e18c024be5fe9711dcbc1cb55f12c1b3ef861
loskyertt <loskyertt0403@gmail.com>
2026-06-05 13:01:00 +0800

    refactor(build): 重构 CMake 构建系统为 GLOB_RECURSE 模式
    主要变更：
    - 顶层 CMakeLists.txt 改为注册 tests/ 和 examples/ 子模块
    - 新增 examples/ 目录，每个 .cpp 自动编译为独立可执行文件
    - 新增 tests/ 目录，每个 .cpp 自动编译为独立可执行文件
    - 移除 core/utils 多模块架构和 GoogleTest 集成

---
+++ tests/test01.cpp
@@ -0,0 +1,6 @@
+#include <print>
+
+int main() {
+  std::println("This is a test01.cpp file!");
+  return 0;
+}
---
+++ tests/test02.cpp
@@ -0,0 +1,6 @@
+#include <print>
+
+int main() {
+  std::println("This is a test02.cpp file!");
+  return 0;
+}
